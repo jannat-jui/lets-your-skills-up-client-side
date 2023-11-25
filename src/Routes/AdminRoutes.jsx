1c85b07d3c8e8c38eae26303b933eaca5ef4ff2e
jannat-jui <jannat15-13735@diu.edu.bd>
2023-11-24 21:08:40 -0800

    create admin routes to check the admin visited routes

---
+++ src/Routes/AdminRoutes.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+
+const AdminRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <progress className="progress w-56"></progress>
+    }
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
+};
+
+export default AdminRoutes;
