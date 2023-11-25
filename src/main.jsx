f91a7cf80a6041918a9f9cc8a6ee7dbcf47aa135
jannat-jui <jannat15-13735@diu.edu.bd>
2023-11-24 22:09:46 -0800

    create sign up page and store the user data in database using tanstack query mutations

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,20 @@ import {
 import router from './Routes/Routes.jsx';
 import AuthProvider from './Provider/AuthProvider';
 import { ThemeProvider } from '@material-tailwind/react';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <ThemeProvider>
    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
     </AuthProvider>
    </ThemeProvider>
   </React.StrictMode>,
